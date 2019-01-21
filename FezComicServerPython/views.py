1fc3a4c2e04e7c7dabb04bf3cb5bd12fb29add70
Rafael Pernil <rafaelpernil@uma.es>
2019-01-21 17:15:06 +0100

    Added Google Sign In api and bugfix

--- FezComicServerPython/views.py
+++ FezComicServerPython/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from .models import Comic,ComicHasSerie,Serie,User,Rol
 from .serializers import ComicSerializer,ComicHasSerieSerializer,SerieSerializer,UserSerializer,RolSerializer
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 class ComicViewSet(viewsets.ModelViewSet):
   queryset = Comic.objects.all()
@@ -8,16 +11,16 @@ class ComicViewSet(viewsets.ModelViewSet):
 
 class ComicHasSerieViewSet(viewsets.ModelViewSet):
   queryset = ComicHasSerie.objects.all()
-  serializer_class = ComicSerializer
+  serializer_class = ComicHasSerieSerializer
 
 class SerieViewSet(viewsets.ModelViewSet):
   queryset = Serie.objects.all()
-  serializer_class = ComicSerializer
+  serializer_class = SerieSerializer
 
 class RolViewSet(viewsets.ModelViewSet):
   queryset = Rol.objects.all()
-  serializer_class = ComicSerializer
+  serializer_class = RolSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
-  serializer_class = ComicSerializer  
+  serializer_class = UserSerializer  
